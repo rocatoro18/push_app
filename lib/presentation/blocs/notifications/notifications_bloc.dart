b82863e015e73d9f16969255b348c87070f65e2d
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2024-01-04 13:10:47 -0700

    Solicitar permisos

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -6,9 +6,24 @@ part 'notifications_event.dart';
 part 'notifications_state.dart';
 
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
   NotificationsBloc() : super(const NotificationsState()) {
     //on<NotificationsEvent>((event, emit) {
     //  // TODO: implement event handler
     //});
+  }
+
+  void requestPermission() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    settings.authorizationStatus;
   }
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,11 +9,14 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Permisos'),
+        title: context.select((NotificationsBloc bloc) => Text(
+              'Permisos ${bloc.state.status}',
+              style: const TextStyle(fontSize: 16),
+            )),
         actions: [
           IconButton(
               onPressed: () {
-                // todo: solicitar permiso de notificaciones
+                context.read<NotificationsBloc>().requestPermission();
               },
               icon: const Icon(Icons.settings))
         ],
